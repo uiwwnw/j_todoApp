4ff7217632b492a74e2b903105cfa8f58f02b073
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-05-22 18:26:02 +0900

    수정

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor() {
+        super(...arguments);
+        
+    }
+    render() {
+        return (
+            <main>
+                dsdasdassajdks
+            </main>
+        );
+    }
+}
